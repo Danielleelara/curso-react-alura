0eb62b0457be6b23a36b7e4203cac384be9a9a6c
Danielle Souza <danielle.andrade@meta.com.br>
2021-09-29 13:07:50 -0300

    feat: criando os métodos

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,22 +6,40 @@ class FormularioCadastro extends Component{
     constructor(){
         super();
         this.titulo="";
+        this.texto="";
     }
-    handleMudancaTitulo(evento){
-       this.titulo = evento.target.value;
-       console.log(this.titulo);
+    _handleMudancaTitulo(evento){
+        evento.stopPropagation();
+        this.titulo = evento.target.value;
+             
+    }
+    _handleMudancaTexto(evento){
+        evento.stopPropagation();
+        this.texto = evento.target.value;
+              
+    }
+    _criarNota(evento) {
+        evento.preventDefault();
+        evento.stopPropagation();
+        console.log(`uma nova nota foi criada ` + this.titulo + " " + this.texto)
+        
     }
     render(){
         return(
-        <form className="form-cadastro">    
+        <form className="form-cadastro" onSubmit={this._criarNota.bind(this)} 
+        
+        >  
             <input type ="text" 
             placeholder= "Título" 
             className="form-cadastro_input" 
-            onChange ={this.handleMudancaTitulo}
+            onChange ={this._handleMudancaTitulo.bind(this)}
             />
             <textarea 
+            rows={15}
             placeholder="Escreva sua nota..." 
-            className="form-cadastro_input"/>
+            className="form-cadastro_input"
+            onChange ={this._handleMudancaTexto.bind(this)}
+            />
             <button className="form-cadastro_input form-cadastro_submit">
                 Criar Nota</button>
         </form>
