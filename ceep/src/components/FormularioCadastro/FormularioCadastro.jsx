7e013f7d2b64435c04656790d40197ee6f10eb76
Danielle Souza <danielle.andrade@meta.com.br>
2021-10-17 19:22:03 -0300

    finalizando o projeto

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,7 +7,21 @@ class FormularioCadastro extends Component{
         super(props);
         this.titulo="";
         this.texto="";
-        this.categoria="";
+        this.categoria="Sem categoria";
+        this.state={categorias:[]};
+        this._novasCategorias = this._novasCategorias.bind(this);
+
+    }
+    componentDidMount(){
+        
+        this.props.categorias.inscrever(this._novasCategorias);
+        
+    }
+    componentWillUnmount(){
+        this.props.categorias.desinscrever(this._novasCategorias);
+    }
+    _novasCategorias(categorias){
+        this.setState({...this.state, categorias})
     }
     _handleMudancaTitulo(evento){
         evento.stopPropagation();
@@ -26,9 +40,10 @@ class FormularioCadastro extends Component{
     _criarNota(evento) {
         evento.preventDefault();
         evento.stopPropagation();
-        this.props.criarNota(this.titulo, this.texto, this.categoria);
-        
+        this.props.criarNota(this.titulo, this.texto, this.categoria); 
     }
+  
+
     render(){
         return(
         <form className="form-cadastro" 
@@ -36,8 +51,8 @@ class FormularioCadastro extends Component{
         >
             <select onChange={this._handleMudancaCategoria.bind(this)} className="form-cadastro_input">
                 <option>Sem Categoria</option>
-                {this.props.categorias.map((categoria)=>{
-                    return <option>{categoria}</option>
+                {this.state.categorias.map((categoria, index)=>{
+                    return <option key={index}>{categoria}</option>
                 })}
             </select>
 
