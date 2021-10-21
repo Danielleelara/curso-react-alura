174cef32b4b4b9e72ddd59562d852f45dcad3937
Danielle Souza <danielle.andrade@meta.com.br>
2021-10-21 18:39:52 -0300

    commit inicial

---
+++ React Fundamentos-Rocketseat/02- Formulário/02-formulario/src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function FormularioCadastro(){
+  
+    return(
+        <form>
+            <input type="text"/>
+        </form>);
+    
+}
+
+export default FormularioCadastro;
