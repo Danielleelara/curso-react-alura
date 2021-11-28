98731a3f49cbb098a727b74f559ac62523bafaea
Danielle Souza <danielle.andrade@meta.com.br>
2021-11-28 18:05:02 -0300

    feat

--- projeto-da-dani/minha-primeira-app/src/Components/PaginaInicial.jsx
+++ projeto-da-dani/minha-primeira-app/src/Components/PaginaInicial.jsx
@@ -4,7 +4,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
 function PaginaInicial(){
   
@@ -13,10 +12,9 @@ function PaginaInicial(){
       <AppBar position="static">
         <Toolbar variant="dense">
           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Modelos
+            </IconButton>
+          <Typography variant="h6" color="Highlight" component="div">
+            Inicio
           </Typography>
         </Toolbar>
       </AppBar>
